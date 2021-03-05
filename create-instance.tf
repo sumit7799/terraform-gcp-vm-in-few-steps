37bd6cb0851aac83b131e4a3785c3c2b506cb6ca
sumit7799 <65368846+sumit7799@users.noreply.github.com>
2021-03-05 14:13:50 +0000

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -1,42 +1,46 @@
-resource "google_compute_instance" "default" {
-  name         = "virtual-machine-from-terraform"
-  machine_type = "f1-micro"
-  zone         = "us-central1-a"
+withCredentials([file(credentialsId: '1bedeb03-6821-4e91-a404-5dfa56b6d721	', variable: 'GC_KEY')]) {
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+    resource "google_compute_instance" "default" {
+    name         = "virtual-machine-from-terraform"
+    machine_type = "f1-micro"
+    zone         = "us-central1-a"
+
+    boot_disk {
+        initialize_params {
+        image = "debian-cloud/debian-9"
+        }
     }
-  }
 
-  network_interface {
+    network_interface {
+        network = "default"
+
+        access_config {
+        // Include this section to give the VM an external ip address
+        }
+    }
+
+        metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+        // Apply the firewall rule to allow external IPs to access this instance
+        tags = ["http-server"]
+    }
+
+    resource "google_compute_firewall" "http-server" {
+    name    = "default-allow-http-terraform"
     network = "default"
 
-    access_config {
-      // Include this section to give the VM an external ip address
+    allow {
+        protocol = "tcp"
+        ports    = ["80"]
     }
-  }
 
-    metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+    // Allow traffic from everywhere to instances with an http-server tag
+    source_ranges = ["0.0.0.0/0"]
+    target_tags   = ["http-server"]
+    }
 
-    // Apply the firewall rule to allow external IPs to access this instance
-    tags = ["http-server"]
-}
+    output "ip" {
+    value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+    }
 
-resource "google_compute_firewall" "http-server" {
-  name    = "default-allow-http-terraform"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  // Allow traffic from everywhere to instances with an http-server tag
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["http-server"]
-}
-
-output "ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
 }
