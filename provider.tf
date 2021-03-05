1461b1e1eefdace6c963194e947ac76f82ff65ac
sumit7799 <65368846+sumit7799@users.noreply.github.com>
2021-03-05 15:21:54 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-$env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\Sumit\Downloads\my-key.json"
 project = "capable-memory-306620"
 region = "us-central1"
 }
