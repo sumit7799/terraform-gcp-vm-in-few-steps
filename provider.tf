499a191819faa4a1d1b99a2167087a6edfde0b99
sumit7799 <65368846+sumit7799@users.noreply.github.com>
2021-03-05 16:48:12 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("./credentials.json")}"
+credentials = "${file("../credentials.json")}"
 project = "capable-memory-306620"
 region = "us-central1"
 }
